7fec1e5dde88ee18f3232f56554fa4e3b11b12e2
MatthewTGM <62822374+MatthewTGM@users.noreply.github.com>
2020-09-10 23:37:00 +0200

    Update invite.js

--- handlers/commands/invite.js
+++ handlers/commands/invite.js
@@ -1,9 +1,12 @@
 const Discord = require('discord.js');
 
 module.exports = {
-	name: 'invite',
-	async execute(message, args, client) {
-		
-		message.channel.send('You can invite me [here](https://top.gg/bot/730063696130211901)');
-	}
+    name: 'invite',
+    async execute(message, args, client) {
+
+        let embed = new Discord.MessageEmbed()
+            .setDescription('You can invite me [here](https://top.gg/bot/730063696130211901)')
+
+        message.channel.send(embed);
+    }
 }
